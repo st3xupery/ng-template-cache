ca27bbe26fdae3278875d103675d16d2aeb9a886
st3xupery <danielalksnis@gmail.com>
2015-03-25 10:18:07 -0400

    first init

---
+++ README.md
@@ -0,0 +1,41 @@
+
+# bowcat
+[![Build Status](https://travis-ci.org/AjayMT/bowcat.svg)](https://travis-ci.org/AjayMT/bowcat)
+
+Quickly concatenate your project's bower dependencies. Like [grunt-bower-concat](http://npmjs.org/grunt-bower-concat) but without the weight and complexity of grunt.
+
+## Installation
+```sh
+$ npm install -g ng-jade-template-cache
+```
+
+## Usage
+```
+bowcat [<input-dir>] [-o <output-dir>] [--min | -m]
+```
+
+`input-dir` is the directory containing the 'bower_components' folder. Defaults to '.'.
+
+`ouput-dir` is the directory to which the concatenated files will be written. Defaults to './build'.
+
+`--min` or `-m` makes bowcat include only minified files (by default, it only includes files listed in the packages' 'main' fields). Defaults to false.
+
+## API
+### bowcat.concatPackage(package, outDir, minified)
+Concatenate a single package. 'package' is the full path to the package, 'outDir' is the output directory, 'minified' is whether or not to include only minified files.
+
+### bowcat.concatPackages(packages, outDir, minified)
+Concatenate a list of packages. 'packages' is the list of full paths of packages to concatenate, 'outDir' is the output directory, 'minified' is whether to include only minified files.
+
+### bowcat.constructFileList(dir, mains, minified)
+Construct a list of files to concatenate for a specific package. 'dir' is the path to the package, 'mains' is the contents of the 'main' field in the package's bower.json, 'minified' is whether or not to include only minified files.
+
+## Logging
+bowcat uses [debug](http://npmjs.org/debug) by TJ Holowaychuk for logging. To turn on logging:
+
+```sh
+$ export DEBUG="bowcat"
+```
+
+## License
+MIT License. See `./LICENSE` for details.
---
+++ ng-template-cache.js
@@ -0,0 +1,76 @@
+var minify = require('html-minifier').minify;
+var jade = require('jade');
+var fs = require('fs');
+
+var opts = require('minimist')(process.argv.slice(2));
+
+var templateFolderPath = opts._[0];
+var outputFilePath = opts.o;
+var templateFileObject = {};
+
+var finalTemplate = function (templates, options) {
+	return 'var angular = require(\'angular\');\n' + 
+	'angular.module("' + options.moduleName + '", []).\n' +
+	'run([\'$templateCache\', function($templateCache) {' +
+	templates + '\n' +
+	'}]);\n';
+};
+
+var options = {
+	moduleName: 'templateCache',
+	quote: '\'',
+	strict: true,
+	indent: '  '
+};
+
+var indent = options.indent;
+
+var putTemplate = function (options) {
+	return '$templateCache.put(' + options.name + ', ' + options.template + ');';
+};
+
+/**
+ * Escape backslashes and the specified quotes and prepend and
+ * append a quote.
+ *
+ * @param {string} The string to quote.
+ * @returns The quoted string.
+ */
+function q(string) {
+	var quote = options.quote;
+	string = string.replace(/\\/, '\\\\');
+	string = string.replace(new RegExp(quote, 'g'), '\\' + quote);
+	return quote + string + quote;
+}
+
+/**
+ * Parse the templates to the putTemplate template.
+ *
+ * @param {templates} An object consisting of key->value pairs
+ *                    of templateName->templateContent.
+ */
+function parsePutTemplate(templates) {
+	var out = '';
+	for (var name in templates) {
+		var tmpl = q(templates[name]);
+		if (tmpl.indexOf('\n') === -1) {
+			tmpl = ' ' + tmpl;
+		} else {
+			var ending = '\n' + indent + indent;
+			tmpl = tmpl.replace(/\n/g, '\\n' + q(' +' + ending));
+			tmpl = ending + tmpl + '\n' + indent;
+		}
+		out += '\n' + indent + putTemplate({
+			name: q(name),
+			template: tmpl
+		});
+	}
+	return out;
+}
+
+fs.readdirSync(templateFolderPath).forEach(function(fileName) {
+	var key = fileName.substr(0, fileName.lastIndexOf('.')) || fileName;
+	templateFileObject[key] = jade.renderFile(templateFolderPath + '/' + fileName);
+});
+
+fs.writeFileSync(outputFilePath, finalTemplate(parsePutTemplate(templateFileObject), options));
